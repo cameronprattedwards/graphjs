399c09237b24765a8154ab840581c46e481229c8
Cameron Edwards <cameronprattedwards@gmail.com>
2014-08-19 10:47:02 -0400

    Add Collection

---
+++ Collection.js
@@ -0,0 +1,16 @@
+(function () {
+	if (typeof define == "function" && define.amd)
+		define(["graphjs/Graph"], factory);
+	else if (typeof exports == "object")
+		module.exports = factory(require("graphjs/Graph"));
+	else
+		root.Collection = factory(root.Graph);
+}(this, function (Graph) {
+	return function Collection(Generic) {
+		return function(array) {
+			return array.map(function (entry) {
+				return Graph.parse(Generic, entry);
+			});
+		}
+	};
+}));
